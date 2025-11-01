7d9fe4b3ab51b8acd0acde985f7a12ccadff306b
gautham1679 <113669202+gautham1679@users.noreply.github.com>
2025-11-01 12:35:13 +0530

    done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Gautham Krishna",
+    author_email="gauthamkrishnaarun8075@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
